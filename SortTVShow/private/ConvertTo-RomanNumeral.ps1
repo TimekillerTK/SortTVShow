6f5b073c5e24bfdde4f6c9db7cd854be4e0cfca1
TimekillerTK <erwartungen@protonmail.com>
2020-10-19 16:01:37 +0200

    optimized ConvertTo-RomanNumerals function

--- SortTVShow/private/ConvertTo-RomanNumeral.ps1
+++ SortTVShow/private/ConvertTo-RomanNumeral.ps1
@@ -7,132 +7,57 @@ function ConvertTo-RomanNumeral {
         $InputNumber
     )
 
+    function PrivateFunc ($Integer,$RomanInteger) {
+
+        if (($InputNumber / $Integer) -ge 1) {
+    
+            # How many times this roman numeral should be printed
+            $TimesRoman = [System.Math]::Floor($InputNumber / $Integer)
+
+            # Return the roman numeral
+            $RomanInteger * $TimesRoman
+
+        } #if
+    
+    }
+
     # Roman "Zero"
     if ($InputNumber -eq 0) {
 
         throw "InvalidInput"
 
     } #if
+
+    # Roman Numerals list
+    $iterations = [PSCustomObject]@{
+        RomanM = @{ a = 1000; b = "M"}
+        RomanD = @{ a = 500; b = "D"}
+        RomanC = @{ a = 100; b = "C"}
+        RomanXC = @{ a = 90; b = "XC"}
+        RomanL = @{ a = 50; b = "L"}
+        RomanXL = @{ a = 40; b = "XL"}
+        RomanX = @{ a = 10; b = "X"}
+        RomanIX = @{ a = 9; b = "IX"}
+        RomanV = @{ a = 5; b = "V"}
+        RomanIV = @{ a = 4; b = "IV"}
+        RomanI = @{ a = 1; b = "I"}
+    } # iterations var
+
+    # All other roman numerals
+    $iterations.PSObject.Properties.Name | ForEach-Object {
     
-    # Roman "C"
-    if (($InputNumber / 100) -ge 1) {
-
-        $numeral = "C"
-        $result = [System.Math]::Floor($InputNumber / 100)
-
-        $outputC = $numeral * $result
-        
-        # Sets the remainder for the next if statement
-        $InputNumber = $InputNumber % 100
-
-    } #if
-
-    # Roman "XC"
-    if (($InputNumber / 90) -ge 1) {
-
-        $numeral = "XC"
-        $result = [System.Math]::Floor($InputNumber / 90)
-
-        $outputXC = $numeral * $result
-        
-        # Sets the remainder for the next if statement
-        $InputNumber = $InputNumber % 90
-
-    } #if
-
-    # Roman "L"
-    if (($InputNumber / 50) -ge 1) {
-
-        $numeral = "L"
-        $result = [System.Math]::Floor($InputNumber / 50)
-
-        $outputL = $numeral * $result
-        
-        # Sets the remainder for the next if statement
-        $InputNumber = $InputNumber % 50
-
-    } #if
-
-    # Roman "XL"
-    if (($InputNumber / 40) -ge 1) {
-
-        $numeral = "XL"
-        $result = [System.Math]::Floor($InputNumber / 40)
-
-        $outputXL = $numeral * $result
-        
-        # Sets the remainder for the next if statement
-        $InputNumber = $InputNumber % 40
-
-    } #if
-
-    # Roman "X"
-    if (($InputNumber / 10) -ge 1) {
-
-        $numeral = "X"
-        $result = [System.Math]::Floor($InputNumber / 10)
-
-        $outputX = $numeral * $result
-        
-        # Sets the remainder for the next if statement
-        $InputNumber = $InputNumber % 10
-
-    } #if
-
-    # Roman "IX"
-    if (($InputNumber / 9) -ge 1) {
+        #Write-Output $iterations.$_['b']
+        #Write-Output $iterations.$_['a']
     
+        $value = PrivateFunc -Integer $iterations.$_['a'] -RomanInteger $iterations.$_['b']
 
-        $numeral = "IX"
-        $result = [System.Math]::Floor($InputNumber / 9)
+        # Set the remainder for the next if statement
+        $InputNumber = $InputNumber % $iterations.$_['a']
 
-        $outputIX = $numeral * $result
-
-        # Sets the remainder for the next if statement
-        $InputNumber = $InputNumber % 9
-    } #if
-
-    # Roman "V"
-    if (($InputNumber / 5) -ge 1) {
+        $finaloutput = $finaloutput + $value
     
-
-        $numeral = "V"
-        $result = [System.Math]::Floor($InputNumber / 5)
-
-        $outputV = $numeral * $result
-
-        # Sets the remainder for the next if statement
-        $InputNumber = $InputNumber % 5
-
-    } #if
-
-    # Roman "IV"
-    if (($InputNumber / 4) -eq 1) {
+    } #Foreach-Object
     
-        $numeral = "IV"
-        $result = [System.Math]::Floor($InputNumber / 4)
-
-        $outputIV = $numeral * $result
-        
-        # Sets the remainder for the next if statement
-        $InputNumber = $InputNumber % 4
-        
-    } #if
-
-    # Roman "I"
-    if (($InputNumber / 1) -ge 1) {
-    
-        $numeral = "I"
-        $result = [System.Math]::Floor($InputNumber / 1)
-
-        $outputI = $numeral * $result
-
-
-    } #if
-
-
-
-    $finaloutput = $outputC + $outputXC + $outputL + $outputXL + $outputX + $outputIX + $outputV + $outputIV + $outputI
-    $finaloutput
-
+    return $finaloutput
+  
 } #function
